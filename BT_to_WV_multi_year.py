a93d03be0ed8b8ef552346d64c566f9a740de2e9
Taylor Wang <taylw065@gmail.com>
2025-11-12 12:33:52 -0500

    cleaned up code and added comments to help future users

--- BT_to_WV_multi_year.py
+++ BT_to_WV_multi_year.py
@@ -13,30 +13,33 @@ import torch.optim as optim
 from matplotlib.ticker import MaxNLocator
 from sklearn.model_selection import train_test_split
 
-#Defines the inputs, activation functions, and layers of the neural network.
+#The neural network is composed of only two layers: the input layer and the output layer. The input layer has 16 neurons,
+#each receving an input from each of the frequencies. The input layer uses the ELU activation function since since it is mostly
+#non negative. The mostly positive ELU function reflects the fact that the vapor values are all positive. 
+
+#--------------- Neural Network Model --------------- 
 class Model(nn.Module):
-	def __init__(self, inp=22, hidden=16, out=1):
+	def __init__(self, num_inputs, num_input_neurons, num_output_neurons, activation_function):
 
 		super().__init__()
 
 		#Hidden layer 1 
-		self.hidden_one = nn.Linear(inp, hidden)
+		self.hidden_one = nn.Linear(num_inputs, num_input_neurons)
 
 		#Hidden layer one activation
-		self.hidden_one_activation = nn.ELU()
+		self.hidden_one_activation = activation_function
 
 		#Output layer
-		self.output = nn.Linear(hidden,out)
+		self.output = nn.Linear(num_input_neurons,num_output_neurons)
 
 		#Initialize weights
 		self._init_weights()
 
-	#The hidden layer has 16 neurons, each of which accepts 22 inputs (corresponding to the 22 brightness temperature frequencies). 
-	#The kaiming initialization sets the 22 weights for each neuron in the hidden layer. The 22 weights for each neuron are then scaled 
-	#by the correlation of their corresponding frequency's brightness temperature signal and the vapor signal.
 	def _init_weights(self):
 
-		#Kaiming Initialization
+		#The neural network learns by determining the error between its predicted output and the actual output
+		#It then corrects weights depending on the output. However, the weights must first be initialized to begin
+		#this process. Kaiming initialization is used to initialize weights in a layer that used the relu activation function
 		for layer in [self.hidden_one]:
 			nn.init.kaiming_normal_(layer.weight, nonlinearity='relu')
 			nn.init.zeros_(layer.bias)
@@ -44,14 +47,16 @@ class Model(nn.Module):
 		nn.init.kaiming_normal_(self.output.weight, nonlinearity='linear')
 		nn.init.zeros_(self.output.bias)
 
-	#Runs the neural network.
+	#Run the neural network
 	def forward(self, inp):
 		hidden_lin_one_out = self.hidden_one(inp)
 		hidden_act_one_out = self.hidden_one_activation(hidden_lin_one_out)
 		output_lin_out = self.output(hidden_act_one_out)
 		return output_lin_out
 
-#Retrieving the brightness temperature data.
+#--------------- Data Loading Functions --------------- 
+
+#Retrieve brightness temperature data
 def load_brightness_data(site, year, level, key, subkey):
     prefix = f"{key}_{subkey}" if level != "lv2" else key
     dir_path = os.path.join(BASE_DIR, str(year), site, level)
@@ -64,7 +69,7 @@ def load_brightness_data(site, year, level, key, subkey):
     
     return pd.to_datetime(dates), values
 
-#Retrieving the vapor data.
+#Retrieve vapor data
 def load_vapor_data(site, year, level, key):
     prefix = f"{key}"
     dir_path = os.path.join(BASE_DIR, str(year), site, level)
@@ -77,10 +82,9 @@ def load_vapor_data(site, year, level, key):
 
     return pd.to_datetime(dates), values
 
-def smooth_array(array, window):
-    return np.convolve(array, np.ones(window)/window, mode='same')
+#--------------- Auxilary Functions --------------- 
 
-#Shortens the larger of either the bright or vapor arrays while maintaining time alignment
+#Matches the length of the brightness temperature and vapor data while maintaining time alignment
 def time_align(bright_dates, bright_vals, vapor_dates, vapor_vals):
 
 	bigger_dates = vapor_dates
@@ -119,8 +123,7 @@ def time_align(bright_dates, bright_vals, vapor_dates, vapor_vals):
 
 	return aligned_dates, aligned_vals
 
-
-
+#Remove specific dates and their corresponding values from the dataset
 def remove_days(dates, vals, days_to_remove): 
 
 	mask = ~dates.normalize().isin(days_to_remove)
@@ -132,7 +135,7 @@ def remove_days(dates, vals, days_to_remove):
 #Creates a training data set and test data set from the brightness temperature and vapor, 
 def prep_train_test_data(site, year, bright_level, vapor_level, freq_list):
 
-	#Loads the brightness temperature data from 22 different frequencies into lists.
+	#List used to hold the brightness temperature data from different freuqencies in one place
 	bright_matrix_vals_1 = []
 	bright_matrix_dates_1 = []
 	bright_matrix_vals_2 = []
@@ -140,36 +143,44 @@ def prep_train_test_data(site, year, bright_level, vapor_level, freq_list):
 	bright_matrix_vals_3 = []
 	bright_matrix_dates_3 = []
 
+	#For loop used to load brightness temperature data from different frequencies of interest
 	for i in range(len(freq_list)):
+
+			#Loads the brigthness temperature data from a specific location, year, and frequency
 			bright_dates_1, bright_vals_1 = load_brightness_data(site, '2022', bright_level, freq_list[i], 'brightness')
 			bright_dates_2, bright_vals_2 = load_brightness_data(site, '2023', bright_level, freq_list[i], 'brightness')
 			bright_dates_3, bright_vals_3 = load_brightness_data(site, '2024', bright_level, freq_list[i], 'brightness')
 
+			#Optional: Remove the dates and their corresponding values from the datasets. This was incorperated into the code to remove RFI
+			'''
 			days_to_remove = pd.to_datetime(['2022-12-22', '2023-04-03', '2023-05-01', '2023-06-14', '2023-06-20', '2023-07-08', '2023-11-14', '2023-11-24', '2023-12-03', '2024-01-16', '2024-02-08', '2024-02-14', '2024-02-15', '2024-02-20', '2024-02-26', '2024-03-12', '2024-03-23', '2024-03-26', '2024-04-09' , '2024-04-15', '2024-05-07', '2024-08-24', '2024-10-22'])
-			'''
 			bright_dates_1, bright_vals_1 = remove_days(bright_dates_1, bright_vals_1, days_to_remove)
-			'''
 			bright_dates_2, bright_vals_2 = remove_days(bright_dates_2, bright_vals_2, days_to_remove)
-			'''
 			bright_dates_3, bright_vals_3 = remove_days(bright_dates_3, bright_vals_3, days_to_remove)
 			'''
-			window = 25
-			bright_vals_1 = smooth_array(bright_vals_1, window)
-			bright_vals_2 = smooth_array(bright_vals_2, window)
-			bright_vals_3 = smooth_array(bright_vals_3, window)
 
-			#remove the first few and last
+			#The brigthness temperature data is very noisy. Training the neural network with noisy brigthness temperature data
+			#causes a noisy neural network output. The noise is reduced by performing a moving average over the data.
+			#The smooth_array function performs this denoising/data smoothening
+			window = 25
+			bright_vals_1 = np.convolve(bright_vals_1, np.ones(window)/window, mode='same')
+			bright_vals_2 = np.convolve(bright_vals_2, np.ones(window)/window, mode='same')
+			bright_vals_3 = np.convolve(bright_vals_3, np.ones(window)/window, mode='same')
+
+			#The moving average is inaccurate at indices that are less than a window length away from the data/array edges.
+			#Near the edges, the moving average attempts to probe outside the array. Points outside the array are automatically considered 0,
+			#so the computed average value at the index of interest is much less than it should be
+			#Therefore, points near the edges are removed from the data to avoid confusing the neural network during training. `
 			bright_dates_1 = bright_dates_1[window:-window]
 			bright_vals_1 = bright_vals_1[window:-window]
 
-			#remove the first few and last
 			bright_dates_2 = bright_dates_2[window:-window]
 			bright_vals_2 = bright_vals_2[window:-window]
 
-			#remove the first few and last
 			bright_dates_3 = bright_dates_3[window:-window]
 			bright_vals_3 = bright_vals_3[window:-window]
 
+			#Add the processed input data into the list 
 			bright_matrix_vals_1.append(bright_vals_1)
 			bright_matrix_dates_1.append(bright_dates_1)
 
@@ -179,6 +190,8 @@ def prep_train_test_data(site, year, bright_level, vapor_level, freq_list):
 			bright_matrix_vals_3.append(bright_vals_3)
 			bright_matrix_dates_3.append(bright_dates_3)
 
+	#Turn the list of data sets at different freqauncies into a matrix
+	#Then turn that matrix into a tensor, which is the format the neural network works in
 	bright_array_1 = np.array(bright_matrix_vals_1)
 	bright_tensor_1 = torch.tensor(bright_array_1, dtype=torch.float32).T
 
@@ -188,19 +201,25 @@ def prep_train_test_data(site, year, bright_level, vapor_level, freq_list):
 	bright_array_3 = np.array(bright_matrix_vals_3)
 	bright_tensor_3 = torch.tensor(bright_array_3, dtype=torch.float32).T
 
-	#Loads the vapor data into lists.
+	#Loads the vapor data from a specific location and year
 	vapor_dates_1, vapor_vals_1 = load_vapor_data(site, '2022', vapor_level, 'Int. Vapor(cm)')
 	vapor_dates_2, vapor_vals_2 = load_vapor_data(site, '2023', vapor_level, 'Int. Vapor(cm)')
 	vapor_dates_3, vapor_vals_3 = load_vapor_data(site, '2024', vapor_level, 'Int. Vapor(cm)')
 
+	#Interference can effect the neural network's training and output.
+	#The remove_days function removes instances of interference from the data
 	'''
 	vapor_dates_1, vapor_vals_1 = remove_days(vapor_dates_1, vapor_vals_1, days_to_remove)
-	'''
 	vapor_dates_2, vapor_vals_2 = remove_days(vapor_dates_2, vapor_vals_2, days_to_remove)
-	'''
 	vapor_dates_3, vapor_vals_3 = remove_days(vapor_dates_3, vapor_vals_3, days_to_remove)
 	'''
-	#Shorten the length of the vapor data to match the length of the brightness temperature data
+
+	#The neural network requires the brigthness temperature data and the vapor data to be the same length
+	#because the brigthness temperature at one time point determines the vapor data at the same time point.
+	#However, the brigthness temperature data and vapor data supplied by NYSM are rarely the same length.
+	#It is necessary to shorten the longer of the two data sets so that they are the same lengths, while 
+	#ensuring the dates at corresponding indices remain nearly identical.
+	#The time align function matches the brightness temperature and vapor lengths and dates.
 	vapor_dates_1, vapor_vals_1 = time_align(bright_matrix_dates_1[0], bright_matrix_vals_1[0], vapor_dates_1, vapor_vals_1)
 	vapor_tensor_1 = torch.tensor(vapor_vals_1, dtype=torch.float32)
 
@@ -210,7 +229,9 @@ def prep_train_test_data(site, year, bright_level, vapor_level, freq_list):
 	vapor_dates_3, vapor_vals_3 = time_align(bright_matrix_dates_3[0], bright_matrix_vals_3[0], vapor_dates_3, vapor_vals_3)
 	vapor_tensor_3 = torch.tensor(vapor_vals_3, dtype=torch.float32)
 
-	#Replace the NaN's in the data with mean values
+
+	#Sometimes the brigthness temperature and vapor data have NaN's, which disrupt further processing in the data.
+	#These NaN's are repalced by the mean value of the data
 	bright_mean_1 = torch.nanmean(bright_tensor_1, dim=0)
 	bright_tensor_1 = torch.where(torch.isnan(bright_tensor_1), bright_mean_1, bright_tensor_1)
 
@@ -231,37 +252,10 @@ def prep_train_test_data(site, year, bright_level, vapor_level, freq_list):
 
 	return bright_dates_1, bright_tensor_1, bright_tensor_2, bright_tensor_3, vapor_tensor_1, vapor_tensor_2, vapor_tensor_3
 
-#Train the model
-def train_model(model, bright_train, vapor_train):
-	
-	#Normalize the brightness temperature input data
-	mean = bright_train.mean(dim=0, keepdim=True)
-	std = bright_train.std(dim=0, keepdim=True)
-	bright_train_norm = (bright_train - mean) / (std + 1e-6)
-
-	#Set up the training settings 
-	optimizer = optim.Adam(model.parameters(), lr=0.001)
-	criterion = nn.MSELoss()
-	epochs = 100
-
-	#Repeat the trianing process 100 times to allow the weights to converge 
-	for i in range(epochs):
-		vapor_pred = model.forward(bright_train_norm)
-
-		vapor_pred = vapor_pred.float()
-		vapor_pred = vapor_pred.view(-1)
-
-		#Measure the error
-		error = criterion(vapor_pred, vapor_train)
-
-		#Change the weights based on the loss value 
-		optimizer.zero_grad()
-		error.backward()
-		optimizer.step()
-
-	return model
-
-#Scales and shifts the neural network's generated vapor signal to match that of the known vapor signal
+#This is still under works
+#The neural network can accurately predict the shape of the output. However, the scale and the offset 
+#rarely match the actual scale and offset of the output. Post processing of the neural network output
+#must be done to properly scale and offset the output. 
 def scale(vapor_eval, vapor_test):
     # Ensure 1D tensor
     vapor_eval = vapor_eval.flatten()
@@ -325,15 +319,41 @@ def scale(vapor_eval, vapor_test):
 
     vapor_eval = vapor_eval * scale
 
-
     return vapor_eval
 
-def calculate_error(vapor_eval, vapor_test):
-	return np.mean(np.abs(vapor_eval - vapor_test))
+#--------------- Train Neural Network --------------- 
 
-#Main
+def train_model(bright_train, vapor_train, optimizer, criterion, epochs):
+	
+	#Normalize the brightness temperature for each frequency. This puts all frequency data sets in the same scale
+	#allowing the model to learn evenly across frequencies. It also reduces the risk of diverging gradients.
 
-#Settings and Constant Values
+	mean = bright_train.mean(dim=0, keepdim=True)
+	std = bright_train.std(dim=0, keepdim=True)
+	bright_train_norm = (bright_train - mean) / (std + 1e-6)
+
+	for i in range(epochs):
+		vapor_pred = model.forward(bright_train_norm)
+
+		vapor_pred = vapor_pred.float()
+		vapor_pred = vapor_pred.view(-1)
+
+		#Measure the error
+		error = criterion(vapor_pred, vapor_train)
+		
+		#Clears old gradients to recompute the gradients
+		optimizer.zero_grad()
+
+		#Comptutes the gradient of the error with respect to each parameter
+		error.backward()
+
+		#Updates the parameters based on the gradient. A larger gradient for a specific parameter
+		#generally indicates the weight associating with that parameter must be changed more. 
+		optimizer.step()
+
+	return model
+
+#--------------- Data Parameters --------------- 
 SITE = 'queens'
 YEAR = '2024'
 BRIGHT_LEVEL = 'lv1'
@@ -342,56 +362,99 @@ BASE_DIR = "./NYS_Mesonet_Data"
 OUTPUT_DIR = "RFI_detection_plots_and_csv"
 FREQ_LIST = [22.234,22.500,23.034,23.834,25.000,26.234,28.000,30.000,51.248,51.760,52.280,52.804,53.336,53.848,54.400,54.940,55.500,56.020,56.660,57.288,57.964,58.800]
 
+#--------------- Neural Network Structure Parameters --------------- 
+
+#The number of inputs is the number of brightness temperature frequencies used to predict the vapor
+NUM_INPUTS = 22
+
+#There is no exact equation to determine the number of input neurons. Too little input neurons,
+#and the neural network is incapable of predicting complex behavior. Too many inpput neurons,
+#and the nueral network runs too slowly. A good rule of thumb is to 
+#pick a value between the number of inputs and the number of outputs, and experiment from there
+NUM_INPUT_NEURONS = 16
+
+#The number of output neurons is the number of output types. Vapor is just one value, so it consist
+#of just one output type
+NUM_OUTPUT_NEURONS = 1
+
+#Activation functions should model the relationship between the input and the output. The brightness temperature vapor
+#are always positive so the activation function should only exhibit change in the first quadrant (positive regions).
+#ELU shows a linear 
+#so the activation function should mostly be positive. And as the brig
+ACTIVATION_FUNCTION = nn.ELU()
+
+#--------------- Create the Neural Network --------------- 
+
 #Create the neural network model
-model = Model()
+model = Model(NUM_INPUTS, NUM_INPUT_NEURONS, NUM_OUTPUT_NEURONS, ACTIVATION_FUNCTION)
+
+#--------------- Neural Network Weight Correction Parameters --------------- 
+
+#The learning rate determines the scale at which the weights are changed. The learning rate should also be small enough
+#such that the weights converge rather than bounce between extreme values. The learning rate should als0 be big enough
+#such that a reasonable number of epochs lead to convergence. 
+
+LEARNING_RATE = 0.001
+
+#The optimizer is the 'rule' for how the weights are changed based on the error.
+#Adam is a well-known optimizer in pytorch that keeps track of the average of past gradients 
+#and adaptive learning rates for each parameter.
+OPTIMIZER = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+#The neural network uses mean squared error as a metric for how far off the predicted output is from the actual output,
+#and how much weight correction is needed to improve the neural network.
+CRITERION = nn.MSELoss()
+
+#Each time the neural network changes its weights, it may undo corrections it previously did. Despite this,
+#the neural network's weights tends to converge to a certain value. By repeating the training multiple times,#the weights convergence becomes more accurate and more consistent
+EPOCHS = 100
+
+#--------------- Main --------------- 
 
 #Seperate the data into training and test datasets
 bright_dates_1, bright_tensor_1, bright_tensor_2, bright_tensor_3, vapor_tensor_1, vapor_tensor_2, vapor_tensor_3 = prep_train_test_data(SITE, YEAR, BRIGHT_LEVEL, VAPOR_LEVEL, FREQ_LIST)
 
-#Train the neural network
-
+#Combine the training data from different years into one dataset
 bright_train = torch.cat([bright_tensor_1, bright_tensor_3], dim=0)
 vapor_train = torch.cat([vapor_tensor_1, vapor_tensor_3], dim=0)
 
-model = train_model(model, bright_train, vapor_train)
+#Train the neural network
+model = train_model(model, bright_train, vapor_train, OPTIMIZER, CRITERION, EPOCHS)
 
-#Test the neural network
 with torch.no_grad():
 
-	bright_dates_sections_1 = np.array_split(bright_dates_1, 5)
-	bright_dates_1 = bright_dates_sections_1[2]
-
-	print(bright_dates_1[0])
-	print(bright_dates_1[-1])
-
+	#Select brightness temperature data to input into the neural network
 	bright_sections = np.array_split(bright_tensor_2, 5)
 	bright_tensor_2 = bright_sections[2]
 
+	#Select the corresponding actual vapor data to compare the predicted vapor output with the actual vapor output
 	vapor_sections = np.array_split(vapor_tensor_2, 5)
 	vapor_tensor_2 = vapor_sections[2]
 
-	vapor_eval = model.forward(bright_tensor_2)
-	vapor_eval = scale(vapor_eval, vapor_tensor_2)
-	vapor_eval = vapor_eval.detach().numpy()
+	#Pass the brightness temperature data into the the neural network to predict the vapor
+	vapor_predicted = model.forward(bright_tensor_2)
 	
-	vapor_test = vapor_tensor_2.detach().numpy()
+	#Properly scale the predicted vapor to match the actual vapor
+	vapor_predicted = scale(vapor_predicted, vapor_tensor_2)
+	
+	#Turn the predicted 
+	vapor_predicted = vapor_eval.detach().numpy()
+	vapor_actual = vapor_tensor_2.detach().numpy()
 
-	print('this is error')
-	print(calculate_error(vapor_eval, vapor_test))
-
-	#Plot the nerual network
+	#Plot the predicted vapor
 	plt.figure()
 	plt.xlabel("Date")
 	plt.ylabel("Vapor (cm)")
 	plt.title("Taylor Neural Network Vapor Output")
-	plt.plot(vapor_eval)
+	plt.plot(vapor_predicted)
 	plt.plot()
 
+	#Plot the actual vapor
 	plt.figure()
 	plt.xlabel("Date")
 	plt.ylabel("Vapor (cm)")
 	plt.title("NYSM Neural Network Output")
-	plt.plot(vapor_tensor_2)
+	plt.plot(vapor__actual)
 	plt.plot()
 
 	plt.show()
